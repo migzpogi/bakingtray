ef70bfe341ae425f9cd845613589c24fd6183b4a
migzpogi <migz.zgim@gmail.com>
2025-11-10 06:58:08 +0800

    Adds json-response and site-map route

--- WebApp.py
+++ WebApp.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response, url_for
+import json
+import socket
 
 from commonlib.initproperties import InitProperties
 
@@ -12,9 +14,42 @@ def stub_test():
     return True
 
 
-@app.route("/")
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route('/')
 def index():
-    return "root"
+    return "Hello World"
+
+
+@app.route("/json-response")
+def json_response():
+    body = {
+        'name': 'John Dela Cruz',
+        'age': 25
+    }
+    response_object = Response(
+        response=json.dumps(body),
+        mimetype='application/json'
+    )
+    response_object.headers['Trace'] = socket.gethostname()
+    return response_object
+
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    return links
 
 
 if __name__ == '__main__':
